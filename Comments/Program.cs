208fda2650554a366a2a82c63d62e13bf67101a5
Tahiba Rahman <105088285+rahmantahiba@users.noreply.github.com>
2023-07-07 14:46:51 -0400

    Practicing creating xml file.

---
+++ Comments/Program.cs
@@ -0,0 +1,25 @@
+using System; 
+
+
+namespace Comments 
+
+{
+    /// XML Documentation
+
+    /// <summary>
+    /// This is the main sample application function.
+    /// </summary>
+    /// <param name="args">An array of string arguments from the command line</param>
+    /// <returns>
+    /// No return value.
+    /// </returns>
+
+
+    class Program {
+        static void Main(String[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+}
+
